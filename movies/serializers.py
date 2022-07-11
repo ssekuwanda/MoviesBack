61273c15a13b141ce87fb4bbb30ac24c34b5a430
ssekuwanda <douglasekuwanda@gmail.com>
2022-07-11 04:42:19 +0300

    added serials

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Downloaded
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'title', 'description','image','file')
+        fields = ('id', 'title', 'description','image','file', )
         model = Movie
+
+class DownloadedSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('user', 'movie' )
+        model = Downloaded
